b3b2b3b37d707c225b788fb57c474aa0787d86d5
Dionne <mylife33@hotmail.com>
2022-05-24 20:36:04 -0500

    wip

---
+++ test-references.js
@@ -0,0 +1,26 @@
+// //this is a sanity check
+// describe("fooFunction", () => {
+
+
+//     //see moudle 2 / objective 6 and module 3 / objective 1 and module 4 objective 1
+//     describe("getArtistByIndex", () => {
+    
+//     //see moudle 2 / objective 4 & 6 and module 3 / objective 1 & 2 and module 4 objective 1
+//     describe("listOfNames", () => {
+    
+//     //see module 2 / objective 4 & 6 and module 3 / objective 1 & 2
+//     //this task is most efficient using splice which would be included in module3-objective2
+//     describe("removeArtist", () => {
+    
+//     //see module 2 / objective 6 and module 3 / objective 2 and module 4 objective 1
+//     describe("addArtist", () => {
+    
+//     //see module 2 / objective 3, 4, & 6 and module 3 / objective 1 & 2 and module 4 objective 1
+//     describe("lotsOfArt", () => {
+    
+//     //see module 2 / objective 3, 4, & 6 and module 3 / objective 1 and module 4 objective 1
+//     describe("artistInfo", () => {
+    
+//     //see module 2 / objective 3, 4, & 6 and module 3 / objective 1 & 2 and module 4 objective 1
+//     describe("artistByCountry", () => {
+    
